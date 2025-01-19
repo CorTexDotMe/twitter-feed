7bc98e70e663a51dd928ce646cf7b46bb5234df7
Danylo Nechyporchuk <danylo.nechyporchuk@gmail.com>
2025-01-19 18:03:12 +0200

    build(services): add build script

---
+++ build.sh
@@ -0,0 +1,3 @@
+docker build -t message-server -f ./cmd/message-server/Dockerfile .
+docker build -t message-consumer -f ./cmd/message-consumer/Dockerfile .
+docker build -t message-bot -f ./cmd/message-bot/Dockerfile .
